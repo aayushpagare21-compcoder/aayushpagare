5728972a729617241f10bd0ee24d8a9cb0e0138a
Aayush Pagare <aayushpagare21@gmail.com>
2025-11-14 23:11:18 +0530

    add opti script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "../global.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
+import Script from "next/script";
 import { Analytics } from "./components/analytics";
 import EmbedmeWidget from "./components/embedmewidget";
 
@@ -24,6 +25,7 @@ export const metadata: Metadata = {
     follow: true,
   },
 };
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -34,19 +36,69 @@ const calSans = LocalFont({
   variable: "--font-calsans",
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
       <head>
         <Analytics />
+
+        {/* === 1. Anti-flicker overlay script === */}
+        <Script id="optimeleon-anti-flicker" strategy="beforeInteractive">
+          {`
+            !(function (h, i, e) {
+              var t = 2000;
+              var n = h.createElement("style");
+              n.id = e;
+              n.innerHTML = "body{opacity:0}";
+              h.head.appendChild(n);
+              i.rmfk = function () {
+                var t = h.getElementById(e);
+                t && t.parentNode.removeChild(t);
+              };
+              setTimeout(i.rmfk, t);
+            })(document, window, "optimeleon-overlay");
+          `}
+        </Script>
+
+        {/* === 2. setOptiCookieConsent helper === */}
+        <Script id="optimeleon-cookie-consent" strategy="beforeInteractive">
+          {`
+            window.setOptiCookieConsent = function(consent) {
+              localStorage.setItem("opti_consent", consent);
+            };
+          `}
+        </Script>
+
+        {/* === 3. Async external script === */}
+        <Script
+          id="optimeleon-external"
+          src="https://cdn-stag.optimeleon.com/aay-z4o6w/aay-z4o72/v1.main.js"
+          async
+          strategy="afterInteractive"
+        />
+
+        {/* === 4. optimeleon init loader snippet === */}
+        <Script id="optimeleon-init" strategy="afterInteractive">
+          {`
+            !function(e,t,o,n,a,c,l){
+              e.optimeleon||(a=e.optimeleon=function(){
+                a.callMethod ? a.callMethod.apply(a,arguments) : a.queue.push(arguments)
+              },
+              a.push=a,
+              a.queue=[],
+              (c=t.createElement(o)).async=!0,
+              c.src="https://cdn-stag.optimeleon.com/aay-z4o6w/aay-z4o72/v1.main.js",
+              (l=t.getElementsByTagName(o)[0]).parentNode.insertBefore(c,l))
+            }(window,document,"script");
+
+            optimeleon("init", true, true);
+          `}
+        </Script>
       </head>
+
       <body
         className={`bg-black ${
-          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          process.env.NODE_ENV === "development" ? "debug-screens" : ""
         }`}
       >
         {children}
